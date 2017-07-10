594151b533a571696f3a6448cf9ec876d2daf177
Theo Dedeken <theo.dedeken@ugent.be>
2017-07-10 14:18:08 +0200

    closes #1

--- src/datascript/fetchdata.sh
+++ src/datascript/fetchdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the latest belgium osm.pbf file from geofabrik
-wget http://download.geofabrik.de/europe/belgium-latest.osm.pbf -P ../mapdata
+wget http://download.geofabrik.de/europe/belgium-latest.osm.pbf -P ../mapdata --backups=1
 
 # Run IDP to convert the osm.pbf to a routerdb
 ./idp/IDP --read-pbf ../mapdata/belgium-latest.osm.pbf --pr --create-routerdb vehicles=../profiles/bicycle.lua --write-routerdb ../mapdata/belgium.routerdb
