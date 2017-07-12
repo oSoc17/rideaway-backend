99d4c49fd3cc3c4365f49800f999818963757e31
Theo Dedeken <theo.dedeken@ugent.be>
2017-07-12 15:21:29 +0200

    add ref and color to instructions

--- src/Models/InstructionProperties.cs
+++ src/Models/InstructionProperties.cs
@@ -10,12 +10,22 @@ namespace rideaway_backend.Model{
 
         public string Color {get; set;}
 
+        public string RouteRef {get; set;}
+
         public DirectionEnum Direction {get; set;}
 
         public RelativeDirection Angle {get; set;}
 
         public InstructionProperties(Instruction Instruction, Instruction Next, Route route){
             this.Instruction = Instruction;
+            Route.Meta meta =  route.ShapeMetaFor(Instruction.Shape);
+            string ColorTemp;
+            meta.Attributes.TryGetValue("color", out ColorTemp);
+            this.Color = ColorTemp;
+            string RefTemp;
+            meta.Attributes.TryGetValue("ref", out RefTemp);
+            this.RouteRef = RefTemp;
+
             if(Next != null){
                 this.Direction = route.DirectionToNext(Instruction.Shape);
                 if (Instruction.Type != "start" && Instruction.Type != "stop"){
