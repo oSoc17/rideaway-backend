a50d08b82720a46be1c5f05879d7482b29f51d7a
Theo Dedeken <theo.dedeken@ugent.be>
2017-12-22 10:53:43 +0100

    upgrade to net core 2.0

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace rideaway_backend {
@@ -12,15 +7,14 @@ namespace rideaway_backend {
         /// Main method of the application. Builds the webhost and runs it.
         /// </summary>
         /// <param name="args">arguments.</param>
-        public static void Main (string[] args) {
-            var host = new WebHostBuilder ()
-                .UseKestrel ()
-                .UseContentRoot (Directory.GetCurrentDirectory ())
-                .UseIISIntegration ()
-                .UseStartup<Startup> ()
-                .Build ();
-
-            host.Run ();
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
